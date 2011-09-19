6af46d27eff12584ebb84cf930e0c23a8c73d0b5
Robert Berry <rjberry@gmail.com>
2011-09-19 23:00:32 +0100

    adding grid class

--- tictactoe.py
+++ tictactoe.py
@@ -32,21 +32,42 @@ class Nought(Sprite):
         Sprite.__init__(self)
         self.image, self.rect = load_image('nought.png')
 
+class Grid(Sprite):
+    ROWS = 3
+    COLS = 3
+    
+    def __init__(self):
+        Sprite.__init__(self)
+        self.image, self.rect = load_image('grid.png')
+        self.data = [[None for col in range(self.COLS)] \
+                     for row in range(self.ROWS)]
+        self.group = pygame.sprite.RenderPlain()
+
+    def get_top_left(self, x, y):
+        if not (0 <= x < self.COLS and 0 <= y < self.ROWS):
+            raise KeyError
+        
+        return (10 + 100 * x, 10 + 100 * y)
+
+    def insert(self, x, y, piece):
+        piece.rect.topleft = self.get_top_left(x, y)
+        self.group.add(piece)
+
+    def draw(self, screen):
+        screen.blit(self.image, (0, 0))
+        self.group.draw(screen)
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode([300, 300])
     pygame.display.set_caption(CAPTION)
 
-    background, background_rect = load_image('grid.png')
-    screen.blit(background, (0,0))
-    pygame.display.flip()
-
     clock = pygame.time.Clock()
 
     x = Cross()
-    x.rect.topleft = (10, 10)
-
-    sprites = pygame.sprite.RenderPlain((x))
+    grid = Grid()
+    grid.insert(0, 0, x)
+    grid.insert(0, 1, Nought())
 
     while True:
         clock.tick(MAXIMUM_FPS)
@@ -57,9 +78,7 @@ def main():
             elif event.type == KEYDOWN and event.key == K_ESCAPE:
                 return
 
-        sprites.update()
-        screen.blit(background, (0, 0))
-        sprites.draw(screen)
+        grid.draw(screen)
         pygame.display.flip()
 
 if __name__ == "__main__": main()
