ba9a98dfd39e043a0d36f64575e38e756c4cf347
Robert Berry <rjberry@gmail.com>
2011-09-20 21:16:41 +0100

    adding AI behaviour

--- tictactoe.py
+++ tictactoe.py
@@ -12,6 +12,8 @@ import pygame
 from pygame.locals import *
 from pygame.sprite import Sprite
 
+from utils import *
+
 # Window caption
 CAPTION = "Noughts & Crosses"
 
@@ -47,7 +49,6 @@ class Nought(Sprite):
 
 class Grid(Sprite):
     """Represents the noughts and crosses grid."""
-    
     def __init__(self):
         Sprite.__init__(self)
         self.image, self.rect = load_image('grid.png')
@@ -73,24 +74,81 @@ class Grid(Sprite):
         screen.blit(self.image, (0, 0))
         self.group.draw(screen)
 
+    def get(self, x, y):
+        return self.data[x][y]
+
     def get_lines(self):
         """Returns lines, which if filled with pieces of one type indicate a
-        winning state."""
-        horizontals = [[self.data[x][y] for y in range(3)] \
+        winning state.
+        """
+        horizontals = [[(x, y) for y in range(3)] \
                        for x in range(3)]
-        verticals = [[self.data[x][y] for x in range(3)] \
+        verticals = [[(x, y) for x in range(3)] \
                      for y in range(3)]
-        diagonals = [[self.data[i][i] for i in range(3)],
-                     [self.data[i][2-i] for i in range(3)]]
+        diagonals = [[(i, i) for i in range(3)],
+                     [(i, 2-1) for i in range(3)]]
         return horizontals + verticals + diagonals
 
-class Computer(object):
-    def __init__(self, grid):
-        self.grid = grid
+class ComputerBehaviourError(Exception): pass
 
-    def get_next_move():
+class Computer(object):
+    """Represents a computer player for tic tac toe."""
+    def __init__(self, grid, piece):
+        self.grid = grid
+        self.piece = piece
+
+    def get_next_move(self):
+        """Uses the grid to figure out the next move the computer wants to
+        make.
+        """
+        enemy_piece = Cross if self.piece is Nought else Nought
+
+        def count_pieces(line):
+            pieces = (self.grid.get(**coord) for coord in line)
+            return (count_if(lambda x: isinstance(x, self.piece), line),
+                    count_if(lambda x: isinstance(x, enemy_piece), line),
+                    count_if(lambda x: x is None, line))
+
+        def first_empty_space(line):
+            for coord in line:
+                if self.grid.get(**coord) == None:
+                    return coord
+            return None
+
+        def win_game(line):
+            mine, enemies, spaces = count_pieces(line)
+            if mine == 2 and spaces = 1:
+                return first_empty_space(line)
+
+        def prevent_losing_game(line):
+            mine, enemies, spaces = count_pieces(line)
+            if enemies == 2 and spaces = 1:
+                return first_empty_space(line)
+
+        def attempt_winning_position(line):
+            mine, enemies, spaces = count_pieces(line)
+            if mine == 1 and spaces == 2:
+                return first_empty_space(line)
+
+        def get_middle_of_line(line):
+            coord = **line[1]
+            if self.grid.get(coord) == None:
+                return coord
+
+        behaviours = (win_game,
+                      prevent_losing_game,
+                      attempt_winning_position,
+                      get_middle_of_line,
+                      first_empty_space)
+        
         lines = self.grid.get_lines()
-        pass
+
+        for behaviour in behaviours:
+            for line in lines:
+                coord = behaviour(line)
+                if coord: return coord
+
+        raise ComputerBehaviourError, "Could not find any available moves"
 
 def main():
     pygame.init()
